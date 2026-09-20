45a4a5abccbf3c6d1a47b9ecaaa269e3afa42043
NEELAPALA NAGA DURGA BHAVANI <bhavanineelapaladev01@gmail.com>
2026-09-20 21:22:57 +0530

    Add file categories configuration
    Defined file categories for various types including images, documents, spreadsheets, presentations, videos, music, archives, Python files, and code files.

---
+++ src/config.py
@@ -0,0 +1,72 @@
+FILE_CATEGORIES = {
+    "Images": {
+        ".jpg",
+        ".jpeg",
+        ".png",
+        ".gif",
+        ".bmp",
+        ".svg",
+        ".webp"
+    },
+
+    "Documents": {
+        ".pdf",
+        ".doc",
+        ".docx",
+        ".txt",
+        ".rtf",
+        ".odt"
+    },
+
+    "Spreadsheets": {
+        ".xls",
+        ".xlsx",
+        ".csv"
+    },
+
+    "Presentations": {
+        ".ppt",
+        ".pptx"
+    },
+
+    "Videos": {
+        ".mp4",
+        ".mkv",
+        ".avi",
+        ".mov",
+        ".wmv",
+        ".flv"
+    },
+
+    "Music": {
+        ".mp3",
+        ".wav",
+        ".aac",
+        ".flac",
+        ".ogg"
+    },
+
+    "Archives": {
+        ".zip",
+        ".rar",
+        ".7z",
+        ".tar",
+        ".gz"
+    },
+
+    "Python": {
+        ".py"
+    },
+
+    "Code": {
+        ".c",
+        ".cpp",
+        ".java",
+        ".js",
+        ".html",
+        ".css",
+        ".json",
+        ".xml",
+        ".sql"
+    }
+}
